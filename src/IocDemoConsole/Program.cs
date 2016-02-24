e53ae31e14d30bc4183de715d90fa53ca57635dc
adam.bezverkov <adam.bezverkov@ezlynx.com>
2016-02-24 12:47:35 -0600

    start

---
+++ src/IocDemoConsole/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IocDemoConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = DoSomeStuff();
+        }
+
+        public static string DoSomeStuff()
+        {
+            string output = null;
+
+            // Get some config data
+
+            // Pull some information from a service
+
+            // Pull some data from the DB
+
+            // Log some data
+
+            // Output the data
+
+            return output;
+        }
+    }
+}
